85184d5a7354a3762b03e900d2251275bebc1df2
Dikshant <dikshantlnaikxyz@gmail.com>
2021-10-17 01:50:29 +0530

    first commit

---
+++ Arrays/1672.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+{
+    int val =0;
+ for (auto i = 0; i < operation.size(); i++)
+ {
+     operation[i][]
+ }
+    
+}
+
+int main(int argc, char const *argv[])
+{
+    string str = "HEllo";
+    for (auto i : str)
+    {
+        cout<<i<<"awdad";
+    }
+    
+    // cout<<str[1];
+}
