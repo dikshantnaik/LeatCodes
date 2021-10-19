8299ae8550c8630951fbb815010639b54aac11bb
Dikshant <dikshantlnaikxyz@gmail.com>
2021-10-20 01:12:11 +0530

    1470 done

---
+++ Arrays/1470.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n = 3;
+    vector<int> arr = {2, 5, 1, 3, 4, 7};
+    vector<int> ans;
+    for (int i = 0; i < n; i++)
+    {
+        ans.push_back(arr[i]);
+    }
+
+    int j = n;
+    for (auto i = 1; i <= ans.size() && j < arr.size(); i += 2)
+    {
+        ans.insert(ans.begin() + i, arr[j]);
+        j++;
+    }
+
+    for (auto k : ans)
+    {
+        cout << k << " ";
+    }
+
+    return 0;
+}
--- Arrays/1672.cpp
+++ Arrays/1672.cpp
@@ -9,18 +9,19 @@ public:
     int maximumWealth(vector<vector<int>> &accounts)
     {
 
-        int res = 0;
+        int ans = 0;
 
         for (auto i = 0; i < accounts.size(); i++)
         {
             int sum = 0;
             for (auto j = 0; j < accounts[i].size(); j++)
             {
+                
                 sum += accounts[i][j];
             }
-            // ans[i] = sum;
-            res = max(res, sum);
+            // Checking which is greater among two
+            ans = max(ans, sum);
         }
-        return res;
+        return ans;
     }
 };
