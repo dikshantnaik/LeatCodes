97fe2f431468ff70a5e7055004acfa15f60e3d91
Dikshant <dikshantlnaikxyz@gmail.com>
2022-04-26 16:21:12 +0530

    Plus Minus

--- CodeShef/test.cpp
+++ CodeShef/test.cpp
@@ -1,15 +1,11 @@
 #include <bits/stdc++.h>
 #include <vector>
 using namespace std;
-int solve()
-{
-}
 
 int main(int argc, char const *argv[])
 {
-    int t;
-    cin >> t;
-    while (t--)
-        solve();
+    string s;
+    cin >> s;
+    cout << s[0];
     return 0;
 }
---
+++ Plus_Minus.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+void plusMinus(vector<int> arr)
+{
+    double NEGATIVE_COUNTER = 0;
+    double POSITIVE = 0;
+    double ZERO = 0;
+    for (auto &&i : arr)
+    {
+
+        if (i > 0)
+        {
+            POSITIVE++;
+        }
+        if (i < 0)
+        {
+            NEGATIVE_COUNTER++;
+        }
+        if (i == 0)
+        {
+            ZERO++;
+        }
+    }
+    cout << double(POSITIVE / arr.size()) << endl;
+    cout << (float)NEGATIVE_COUNTER / (float)arr.size() << endl;
+    cout << (float)ZERO / (float)arr.size() << endl;
+}
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    plusMinus(arr);
+
+    return 0;
+}
+
+string ltrim(const string &str)
+{
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace))));
+
+    return s;
+}
+
+string rtrim(const string &str)
+{
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end());
+
+    return s;
+}
+
+vector<string> split(const string &str)
+{
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos)
+    {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
